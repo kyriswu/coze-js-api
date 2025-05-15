74f9787d7f8ccd9c40731408a1111e33932b8318
wucheng <magiclaw@qq.com>
2025-05-15 10:54:38 +0800

    更新代理插件

--- index.js
+++ index.js
@@ -513,17 +513,18 @@ app.post('/jina_reader', async (req, res) => {
     try {
         const response = await axios.get(`https://r.jina.ai/${url}`, {
             headers: {
-            'Authorization': 'Bearer jina_4631fe20e0fc408aafba69e4c1ddbb5fNzIjfDfnxo1y6sOgO8d3mbXKGo3l'
+            'Authorization': 'Bearer jina_4631fe20e0fc408aafba69e4c1ddbb5fNzIjfDfnxo1y6sOgO8d3mbXKGo3l',
+            'X-Proxy-Url': 'http://umwhniat-rotate:eudczfs5mkzt@p.webshare.io:80'
             },
-            proxy: {
-                protocol: 'http',
-                host: 'p.webshare.io',
-                port: 80,
-                auth: {
-                    username: 'umwhniat-rotate',
-                    password: 'eudczfs5mkzt'
-                }
-            }
+            // proxy: {
+            //     protocol: 'http',
+            //     host: 'p.webshare.io',
+            //     port: 80,
+            //     auth: {
+            //         username: 'umwhniat-rotate',
+            //         password: 'eudczfs5mkzt'
+            //     }
+            // }
         });
         res.send({
             code: 0,
