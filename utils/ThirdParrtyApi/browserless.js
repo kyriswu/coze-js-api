bc9ffc19acd6deb3b9fbc093306ebea76eb40015
wucheng <magiclaw@qq.com>
2025-06-24 16:03:28 +0800

    新增国内代理IP池

--- utils/ThirdParrtyApi/browserless.js
+++ utils/ThirdParrtyApi/browserless.js
@@ -97,9 +97,7 @@ const browserless = {
             console.error('Error in chromium_content:', error);
             return null
         } finally {
-            const pid = browser.process().pid;
             await browser.close();
-            kill(pid, 'SIGKILL');
         }
         
         try {
