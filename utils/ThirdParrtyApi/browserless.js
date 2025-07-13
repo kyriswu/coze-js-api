934be1d9ad68bde747974c8a903469d9a6493bfb
wucheng <magiclaw@qq.com>
2025-07-13 19:33:51 +0800

    添加firecrawl

--- utils/ThirdParrtyApi/browserless.js
+++ utils/ThirdParrtyApi/browserless.js
@@ -192,6 +192,8 @@ const browserless = {
                 throw new Error(`HTTP request failed with status ${response.status()}`);
             }
 
+            setTimeout(() => {}, 1000);
+
             if (opt && opt.element_type && opt.element) {
                 if (opt.element_type === 'xpath') {
                     console.log("等待xpath元素：", opt.element)
