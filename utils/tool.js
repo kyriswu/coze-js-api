628a28f4b09bc6e6347c9fda2a8cfbde838ded82
wucheng <magiclaw@qq.com>
2025-07-15 14:46:57 +0800

    公众号文章搜索

--- utils/tool.js
+++ utils/tool.js
@@ -30,7 +30,7 @@ const tool = {
 
         // 增加特殊域名列表，命中则走国内代理逻辑
         const chinaDomainList = [
-            'tophub.today'
+            'tophub.today','qunar.com'
             // 可继续添加更多域名
         ];
         const urlObj = new URL(url);
