fe6e2a4793fc0ee85fa20fa8d8fe82dc8f93c699
wucheng <magiclaw@qq.com>
2025-07-25 13:32:52 +0800

    api_key收费机制

--- index.js
+++ index.js
@@ -675,18 +675,18 @@ app.post('/google/search/web', async (req, res) => {
             }); 
         }
     }else{
-        const canSearch = await canSearchGoogle(free_key);
-        if (!canSearch) {
-            return res.send({
-                code: 0,
-                msg: '为了保证付费用户的使用体验，免费用户有使用频率限制，请联系作者购买api_key！【B站:小吴爱折腾】',
-                data: [{
-                    'title': '免费用户有频率限制，1小时内使用1次，付费购买api_key，请联系作者！【B站:小吴爱折腾】',
-                    'link': 'https://space.bilibili.com/396762480',
-                    'snippet': '免费用户有频率限制，1小时内使用1次，付费购买api_key，请联系作者！【B站:小吴爱折腾】'
-                }]
-            }); 
-        }
+        // const canSearch = await canSearchGoogle(free_key);
+        // if (!canSearch) {
+        //     return res.send({
+        //         code: 0,
+        //         msg: '为了保证付费用户的使用体验，免费用户有使用频率限制，请联系作者购买api_key！【B站:小吴爱折腾】',
+        //         data: [{
+        //             'title': '免费用户有频率限制，1小时内使用1次，付费购买api_key，请联系作者！【B站:小吴爱折腾】',
+        //             'link': 'https://space.bilibili.com/396762480',
+        //             'snippet': '免费用户有频率限制，1小时内使用1次，付费购买api_key，请联系作者！【B站:小吴爱折腾】'
+        //         }]
+        //     }); 
+        // }
     }
 
     // search1api.search(q).then(async (data) => {
