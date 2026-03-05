a1b86fae7bac02b649e642da4b747a0d7c045507
wucheng <magiclaw@qq.com>
2026-03-05 19:02:09 +0800

    test

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 git pull
-docker compose down
-docker rmi my-custom-node-python-app
-docker compose up --build -d
+if command -v podman >/dev/null 2>&1; then
+	podman compose down
+	podman rmi my-custom-node-python-app
+	podman compose up --build -d
+else
+	docker compose down
+	docker rmi my-custom-node-python-app
+	docker compose up --build -d
+fi
